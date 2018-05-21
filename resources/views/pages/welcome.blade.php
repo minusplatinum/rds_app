76b12965bf94bd2979eb478964cda41ec7c608fb
Ryan Starks <starksryan55@gmail.com>
2018-05-21 07:08:11 -0400

    Update welcome.blade.php

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -7,32 +7,30 @@
             <div class="carousel-inner" style="background-color:#f4f4f4;">
                 <div class="carousel-item active">
                     <div style="min-height:300px;" class="welcomeSlide1 d-flex flex-column text-center py-4">
-                        <h1 class="display-4">Welcome To RDS Web Design!</h1>
+                        <h1 class="display-4">Welcome To RDS Web Designs!</h1>
                         <p class="lead text-muted font-weight-bold mb-0">Where We Focus On Coding Your Business's Future</p>
                         <p class="lead text-muted font-weight-bold"> For Online Success!</p>
                     </div>
                 </div>
                 <div class="carousel-item">
                     <div style="min-height:300px;" class="welcomeSlide2 text-center py-3">
-                        <h1 class="display-4">Basic Website Packages</h1>
-                        <p class="lead text-muted font-weight-bold mx-3 my-0">Learn why our Basic Website Package isnt so basic!</p>
-                        
+                        <h1 class="display-4">Check Out Our Website Packages!</h1>
+                        <p class="lead text-muted font-weight-bold mx-3 my-0">Website Hosting, Domain Name & Custom Email Included</p>
                         <a class="btn btn-success btn-lg" href="#" role="button">Learn More!</a>
                     </div>
                 </div>
                 <div class="carousel-item">
                     <div style="min-height:300px;" class="welcomeSlide3 text-center py-3">
-                        <h1 class="display-4">Premium Website Packages</h1>
-                        <p class="lead text-muted font-weight-bold mx-3 my-0">starting at just</p>
-                        <h1>$299</h1>
+                        <h1 class="display-4">Looking For Awesome Website Features & Plugins?</h1>
+                        <p class="lead text-muted font-weight-bold mx-3 my-0">We Have A Wide Range Of Modern Website Plugins & Custom Features!</p>
                         <a class="btn btn-success btn-lg" href="#" role="button">Learn More!</a>
                     </div>
                 </div>
                 <div class="carousel-item">
                     <div style="min-height:300px;" class="welcomeSlide4 text-center py-3">
-                        <h1 class="display-4">Website Application Packages</h1>
-                        <p class="lead text-muted font-weight-bold mx-3 my-0">starting at just</p>
-                        <h1>$499</h1>
+                        <h1 class="display-4">Need More Then Just A Website?</h1>
+                        <p class="lead text-muted font-weight-bold mx-3 my-0">Ask Us About Our Website Applications</p>
+                        <p class="lead text-muted font-weight-bold mx-3 my-0">We Only Use The Best Frameworks & Libraries!</p>
                         <a class="btn btn-success btn-lg" href="#" role="button">Learn More!</a>
                     </div>
                 </div>    
@@ -41,7 +39,7 @@
         <!-- Call to Action Well -->
         <div class="card text-white bg-primary my-4 text-center">
             <div class="card-body">
-                <p class="text-white m-0 lead">Looking for a website to help grow your business online? Check out one of the packages below to get started!</p>
+                <p class="text-white m-0 lead">Looking for a website to help grow your business online? Check out one of the packages below to Learn More!</p>
             </div>
         </div>
     </div><!-- container end -->
@@ -51,7 +49,7 @@
             <div data-aos="fade-right" class="col-lg-4 mb-4">
                 <div class="card h-100">
                     <div class="card-header">
-                            <h2 class="card-title ">Basic Website Package</h2>
+                            <h2 class="card-title ">Starter Website Package</h2>
                     </div>
                     <div class="card-body">
                         <p class="card-text">Looking for a low cost solution for getting your business online? Then the basic website package is a perfect choice. Starting at just $99 you can have a responsive website thats</p>
@@ -64,7 +62,7 @@
                         </ul>
                     </div>
                     <div class="card-footer">
-                        <a href="#" class="btn btn-lg btn-block btn-success">More Info</a>
+                        <a href="#" class="btn btn-lg btn-block btn-success">Learn More!</a>
                     </div>
                 </div>
             </div>
@@ -72,7 +70,7 @@
             <div data-aos="fade-up" class="col-lg-4 mb-4">
                 <div class="card h-100">
                         <div class="card-header">
-                                <h2 class="card-title">Premium Website Package</h2>
+                                <h2 class="card-title">Basic Website Package</h2>
                         </div>
                     <div class="card-body">
                         
@@ -94,7 +92,7 @@
             <div data-aos="fade-left" class="col-lg-4 mb-4">
                 <div class="card h-100">
                         <div class="card-header">
-                                <h2 class="card-title">Website Application Package</h2>
+                                <h2 class="card-title">Premium Website Package</h2>
                         </div>
                     <div class="card-body">
                         
@@ -189,4 +187,4 @@
         </div>
     </div><!-- container end -->
 </div>
-@endsection
+@endsection 
