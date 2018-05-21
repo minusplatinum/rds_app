d812499b75f2d8c32d7e6a124391e972c17f2bf8
Ryan Starks <starksryan55@gmail.com>
2018-05-21 17:57:48 -0400

    Update packages.blade.php

--- resources/views/pages/packages.blade.php
+++ resources/views/pages/packages.blade.php
@@ -11,7 +11,7 @@
       <div class="row">
 
         <div class="col-md-8">
-          <img class="img-fluid" src="http://placehold.it/750x500" alt="">
+          <img class="img-fluid" src="http://placehold.it/500x500" alt="">
         </div>
 
         <div class="col-md-4">
