2962a41409e1cf211bf5f46366ad870c9a7c1d6e
Ryan Starks <starksryan55@gmail.com>
2018-05-21 17:58:43 -0400

    Update packages.blade.php

--- resources/views/pages/packages.blade.php
+++ resources/views/pages/packages.blade.php
@@ -10,11 +10,11 @@
       <!-- Portfolio Item Row -->
       <div class="row">
 
-        <div class="col-md-8">
+        <div class="col-md-6">
           <img class="img-fluid" src="http://placehold.it/500x500" alt="">
         </div>
 
-        <div class="col-md-4">
+        <div class="col-md-6">
           <h3 class="my-3">Project Description</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae. Sed dui lorem, adipiscing in adipiscing et, interdum nec metus. Mauris ultricies, justo eu convallis placerat, felis enim.</p>
           <h3 class="my-3">Project Details</h3>
