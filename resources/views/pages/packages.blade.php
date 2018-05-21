8678066a43fc9f737c62b8535e0265dc8c79df87
Ryan Starks <starksryan55@gmail.com>
2018-05-21 10:34:15 -0400

    Update packages.blade.php

--- resources/views/pages/packages.blade.php
+++ resources/views/pages/packages.blade.php
@@ -23,6 +23,54 @@
                 </div>
             </div>
         </div>
-    </div>
-</div><!-- /.container -->
-@endsection 
+    </div> <!-- row end -->
+    
+    <div class="row px-3">
+        <div class="card">
+            <div class="row">
+                <h3>Basic Website Package</h3>
+            </div>
+            <div class="row">
+                <div class="col-lg-4">
+                    <img class="img-fluid img-rounded img-thumbnail" src="http://via.placeholder.com/350x250" alt="Starter Package Image">
+                </div>
+                <div class="col-lg-8">
+                    <p>The Starter Website Package is a great starting point for individuals and small businesses to expand to the online world. It includes all the basic tools and features to make a modern responsive website.</p>
+                    <ul>
+                        <li>Mobile Responsive Website Designs</li>
+                        <li>Includes Up To 5 Website Pages</li>
+                        <li>Image Galleries And Content Sliders</li>
+                        <li>Customer Inquiry And Contact Forms</li>
+                        <li>Custom Website Email Accounts</li>
+                    </ul>
+                    <a class="btn btn-lg btn-success" href="#">Get Started</a>
+                </div>
+            </div>
+        </div>
+    </div> <!-- row end -->
+    
+    <div class="row px-3">
+        <div class="card">
+            <div class="row">
+                <h3>Premium Website Package</h3>
+            </div>
+            <div class="row">
+                <div class="col-lg-4">
+                    <img class="img-fluid img-rounded img-thumbnail" src="http://via.placeholder.com/350x250" alt="Starter Package Image">
+                </div>
+                <div class="col-lg-8">
+                    <p>The Starter Website Package is a great starting point for individuals and small businesses to expand to the online world. It includes all the basic tools and features to make a modern responsive website.</p>
+                    <ul>
+                        <li>Mobile Responsive Website Designs</li>
+                        <li>Includes Up To 5 Website Pages</li>
+                        <li>Image Galleries And Content Sliders</li>
+                        <li>Customer Inquiry And Contact Forms</li>
+                        <li>Custom Website Email Accounts</li>
+                    </ul>
+                    <a class="btn btn-lg btn-success" href="#">Get Started</a>
+                </div>
+            </div>
+        </div>
+    </div> <!-- row end -->
+</div><!-- container end -->
+@endsection
