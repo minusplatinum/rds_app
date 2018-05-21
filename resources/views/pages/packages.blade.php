18ee58c3ff1c454e35f340dd284b355a8a283901
Ryan Starks <starksryan55@gmail.com>
2018-05-21 18:12:48 -0400

    Update packages.blade.php

--- resources/views/pages/packages.blade.php
+++ resources/views/pages/packages.blade.php
@@ -1,22 +1,75 @@
 @extends('layouts.app')
 @section('content')
 <div class="container">
-      <div class="row"> 
-            <div class="col-md-6">
-                  <img class="img-fluid" src="http://placehold.it/500x500" alt="">
-            </div>
+    <div class="row"> 
+        <div>
+            <h3 class="my-3">Starter Website Package</h3>
+        </div>
 
-            <div class="col-md-6">
-                  <h3 class="my-3">Project Description</h3>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae. Sed dui lorem, adipiscing in adipiscing et, interdum nec metus. Mauris ultricies, justo eu convallis placerat, felis enim.</p>
-                  <h3 class="my-3">Project Details</h3>
-                  <ul>
-                        <li>Lorem Ipsum</li>
-                        <li>Dolor Sit Amet</li>
-                        <li>Consectetur</li>
-                        <li>Adipiscing Elit</li>
-                 </ul>
+        <div class="col-md-6">
+              <img class="img-fluid" src="http://placehold.it/500x500" alt="">
+        </div>
+
+        <div class="col-md-6">
+            <p>The Starter Website Package is a great starting point for individuals and small businesses to expand to the online world. It includes all the basic tools and features to make a modern responsive website.</p>
+            <ul>
+                <li>Mobile Responsive Website Designs</li>
+                <li>Includes Up To 5 Website Pages</li>
+                <li>Image Galleries And Content Sliders</li>
+                <li>Customer Inquiry And Contact Forms</li>
+                <li>Custom Website Email Accounts</li>
+            </ul>
+            <a class="btn btn-lg btn-success btn-block d-md-inline" href="#">Get Started</a>
+        </div>
+    </div><!-- /.row -->
+    
+    <div class="row px-3">
+        <div class="card">
+            <div class="row m-auto">
+                <h3>Basic Website Package</h3>
             </div>
-      </div><!-- /.row -->
+            <div class="row">
+                <div class="col-lg-4">
+                    <img class="img-fluid img-rounded img-thumbnail" src="http://via.placeholder.com/350x250" alt="Starter Package Image">
+                </div>
+                <div class="col-lg-8">
+                    <p>The Starter Website Package is a great starting point for individuals and small businesses to expand to the online world. It includes all the basic tools and features to make a modern responsive website.</p>
+                    <ul>
+                        <li>Mobile Responsive Website Designs</li>
+                        <li>Includes Up To 5 Website Pages</li>
+                        <li>Image Galleries And Content Sliders</li>
+                        <li>Customer Inquiry And Contact Forms</li>
+                        <li>Custom Website Email Accounts</li>
+                    </ul>
+                    <a class="btn btn-lg btn-success" href="#">Get Started</a>
+                </div>
+            </div>
+        </div>
+    </div> <!-- row end -->
+    
+    <div class="row px-3">
+        <div class="card">
+            <div class="row m-auto">
+                <h3>Premium Website Package</h3>
+            </div>
+            <div class="row">
+                <div class="col-lg-4">
+                    <img class="img-fluid img-rounded img-thumbnail" src="http://via.placeholder.com/350x250" alt="Starter Package Image">
+                </div>
+                <div class="col-lg-8">
+                    <p>The Starter Website Package is a great starting point for individuals and small businesses to expand to the online world. It includes all the basic tools and features to make a modern responsive website.</p>
+                    <ul>
+                        <li>Mobile Responsive Website Designs</li>
+                        <li>Includes Up To 5 Website Pages</li>
+                        <li>Image Galleries And Content Sliders</li>
+                        <li>Customer Inquiry And Contact Forms</li>
+                        <li>Custom Website Email Accounts</li>
+                    </ul>
+                    <a class="btn btn-lg btn-success" href="#">Get Started</a>
+                </div>
+            </div>
+        </div>
+    </div> <!-- row end -->
 </div><!-- container end -->
 @endsection
+
