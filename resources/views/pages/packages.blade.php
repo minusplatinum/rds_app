64f7938d813bd6e5878a6c99d616e60ea64e25e6
= <=>
2018-05-23 20:01:38 -0400

    page updates

--- resources/views/pages/packages.blade.php
+++ resources/views/pages/packages.blade.php
@@ -1,73 +1,74 @@
 @extends('layouts.app')
 @section('content')
-<div class="container">
-    <div class="m-auto">
-        <h3 class="my-3">Starter Website Package</h3>
-    </div>
-    <div class="row"> 
-        <div class="col-md-5 m-auto">
-              <img class="img-fluid" src="http://placehold.it/500x250" alt="">
+<div class="container-fluid py-5 packagesImage">
+    <div class="container serviceContainer">
+        <div class="m-auto text-sm-center text-md-left text-lg-center">
+            <h3 class="my-3">Starter Website Package</h3>
         </div>
-
-        <div class="col-md-7">
-            <p>The Starter Website Package is a great starting point for individuals and small businesses to expand to the online world. It includes all the basic tools and features to make a modern responsive website.</p>
-            <ul>
-                <li>Mobile Responsive Website Designs</li>
-                <li>Includes Up To 5 Website Pages</li>
-                <li>Image Galleries And Content Sliders</li>
-                <li>Customer Inquiry And Contact Forms</li>
-                <li>Custom Website Email Accounts</li>
-            </ul>
-            <a class="btn btn-lg btn-success btn-block d-md-inline" href="#">Get Started</a>
-        </div>
-    </div><!-- /.row -->
-    <div class="row px-3">
-        <div class="card">
-            <div class="row m-auto">
-                <h3>Basic Website Package</h3>
+        <div class="row"> 
+            <div class="col-md-5 m-auto">
+                  <img class="img-fluid" src="http://placehold.it/700x500" alt="">
             </div>
-            <div class="row">
-                <div class="col-lg-4">
-                    <img class="img-fluid img-rounded img-thumbnail" src="http://via.placeholder.com/350x250" alt="Starter Package Image">
-                </div>
-                <div class="col-lg-8">
-                    <p>The Starter Website Package is a great starting point for individuals and small businesses to expand to the online world. It includes all the basic tools and features to make a modern responsive website.</p>
-                    <ul>
-                        <li>Mobile Responsive Website Designs</li>
-                        <li>Includes Up To 5 Website Pages</li>
-                        <li>Image Galleries And Content Sliders</li>
-                        <li>Customer Inquiry And Contact Forms</li>
-                        <li>Custom Website Email Accounts</li>
-                    </ul>
-                    <a class="btn btn-lg btn-success" href="#">Get Started</a>
-                </div>
-            </div>
-        </div>
-    </div> <!-- row end -->
     
-    <div class="row px-3">
-        <div class="card">
-            <div class="row m-auto">
-                <h3>Premium Website Package</h3>
+            <div class="col-md-7">
+                <p>The Starter Website Package is a great starting point for individuals and small businesses to expand to the online world. It includes all the basic tools and features to make a modern responsive website.</p>
+                <ul>
+                    <li>Mobile Responsive Website Designs</li>
+                    <li>Includes Up To 5 Website Pages</li>
+                    <li>Image Galleries And Content Sliders</li>
+                    <li>Customer Inquiry And Contact Forms</li>
+                    <li>Custom Website Email Accounts</li>
+                </ul>
+                <a class="btn btn-lg btn-success btn-block d-md-inline" href="#">Get Started</a>
             </div>
-            <div class="row">
-                <div class="col-lg-4">
-                    <img class="img-fluid img-rounded img-thumbnail" src="http://via.placeholder.com/350x250" alt="Starter Package Image">
-                </div>
-                <div class="col-lg-8">
-                    <p>The Starter Website Package is a great starting point for individuals and small businesses to expand to the online world. It includes all the basic tools and features to make a modern responsive website.</p>
-                    <ul>
-                        <li>Mobile Responsive Website Designs</li>
-                        <li>Includes Up To 5 Website Pages</li>
-                        <li>Image Galleries And Content Sliders</li>
-                        <li>Customer Inquiry And Contact Forms</li>
-                        <li>Custom Website Email Accounts</li>
-                    </ul>
-                    <a class="btn btn-lg btn-success" href="#">Get Started</a>
-                </div>
-            </div>
+        </div><!-- /.row -->        
+    </div>
+<hr>
+    <div class="container serviceContainer py-3">
+        <div class="m-auto">
+            <h3 class="my-3">Basic Website Package</h3>
         </div>
-    </div> <!-- row end -->
+        <div class="row"> 
+            <div class="col-md-5 m-auto">
+                  <img class="img-fluid" src="http://placehold.it/700x500" alt="">
+            </div>
+    
+            <div class="col-md-7">
+                <p>The Starter Website Package is a great starting point for individuals and small businesses to expand to the online world. It includes all the basic tools and features to make a modern responsive website.</p>
+                <ul>
+                    <li>Mobile Responsive Website Designs</li>
+                    <li>Includes Up To 5 Website Pages</li>
+                    <li>Image Galleries And Content Sliders</li>
+                    <li>Customer Inquiry And Contact Forms</li>
+                    <li>Custom Website Email Accounts</li>
+                </ul>
+                <a class="btn btn-lg btn-success btn-block d-md-inline" href="#">Get Started</a>
+            </div>
+        </div><!-- /.row -->        
+    </div>
+<hr>
+    <div class="container serviceContainer py-3">
+        <div class="m-auto">
+            <h3 class="my-3">Premium Website Package</h3>
+        </div>
+        <div class="row"> 
+            <div class="col-md-5 m-auto">
+                  <img class="img-fluid" src="http://placehold.it/700x500" alt="">
+            </div>
+    
+            <div class="col-md-7">
+                <p>The Starter Website Package is a great starting point for individuals and small businesses to expand to the online world. It includes all the basic tools and features to make a modern responsive website.</p>
+                <ul>
+                    <li>Mobile Responsive Website Designs</li>
+                    <li>Includes Up To 5 Website Pages</li>
+                    <li>Image Galleries And Content Sliders</li>
+                    <li>Customer Inquiry And Contact Forms</li>
+                    <li>Custom Website Email Accounts</li>
+                </ul>
+                <a class="btn btn-lg btn-success btn-block d-md-inline" href="#">Get Started</a>
+            </div>
+        </div><!-- /.row -->        
+    </div>
 </div><!-- container end -->
 @endsection
 
--- resources/views/pages/templates.blade.php
+++ resources/views/pages/templates.blade.php
@@ -1,59 +1,61 @@
 @extends('layouts.app')
 @section('content')
 <!-- Page Content -->
-<div class="container">
-    <div class="row">
-        <div class="m-auto">
-            <h1 class="py-4">Featured Template</h1>
-        </div>
-        <div class="card p-3">
-            <div class="row">
-                <div class="col-lg-4">
-                    <img img class="img-fluid img-thumbnail p-2" src="{{ $templates[3]['imgUrl'] . $templates[3]['img1Name'] }}" alt="{{ $templates[3]['name'] }}">
-                </div>
-                <div class="col-lg-8 py-3 text-center text-lg-left">
-                    <h4>{{ $templates[3]['name'] }}</h4>
-                    <p>{{ $templates[3]['description'] }}</p>
-                    <div class="row">
-                        <div class="col-lg-6">
-                            <ul class="list-unstyled">
-                                <li>{{ $templates[3]['feature1'] }}</li>
-                                <li>{{ $templates[3]['feature2'] }}</li>
-                                <li>{{ $templates[3]['feature3'] }}</li>
-                                <li>{{ $templates[3]['feature4'] }}</li>
-                            </ul>
-                        </div>
-                        <div class="col-lg-6 d-flex align-items-end justify-content-around">
-                            <a class="btn btn-block btn-lg btn-success mr-2" href="{{ route('register') }}">Get Started!</a>
-                            <a class="btn btn-block btn-lg btn-info mr-2" href="{{ url('templates/' . $templates[3]['id']) }}">Learn More!</a>
+<div class="container-fluid py-5 templatesImage">
+    <div class="container">
+        <div class="row templateOpacity">
+            <div class="m-auto">
+                <h1 class="py-4">Featured Template</h1>
+            </div>
+            <div class="card p-3">
+                <div class="row">
+                    <div class="col-lg-4">
+                        <img img class="img-fluid img-thumbnail p-2" src="{{ $templates[3]['imgUrl'] . $templates[3]['img1Name'] }}" alt="{{ $templates[3]['name'] }}">
+                    </div>
+                    <div class="col-lg-8 py-3 text-center text-lg-left">
+                        <h4>{{ $templates[3]['name'] }}</h4>
+                        <p>{{ $templates[3]['description'] }}</p>
+                        <div class="row">
+                            <div class="col-lg-6">
+                                <ul class="list-unstyled">
+                                    <li>{{ $templates[3]['feature1'] }}</li>
+                                    <li>{{ $templates[3]['feature2'] }}</li>
+                                    <li>{{ $templates[3]['feature3'] }}</li>
+                                    <li>{{ $templates[3]['feature4'] }}</li>
+                                </ul>
+                            </div>
+                            <div class="col-lg-6 d-flex align-items-end justify-content-around">
+                                <a class="btn btn-block btn-lg btn-success mr-2" href="{{ route('register') }}">Get Started!</a>
+                                <a class="btn btn-block btn-lg btn-info mr-2" href="{{ url('templates/' . $templates[3]['id']) }}">Learn More!</a>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </div><!-- /.row -->
-    <br>
-    <div class="row my-3">
-        <div class="card p-3">
-            <div class="row">
-                @foreach($templates as $template)
-                <div class="col-lg-4 my-2">
-                    <div class="card h-100">
-                        <div class="card-header">
-                            <img img class="img-fluid img-thumbnail" src="{{ $template->imgUrl . $template->img1Name }}" alt="{{ $template->name }}">
-                        </div>
-                        <div class="card-body">
-                            <h4 class="card-title text-center">{{ $template->name }}</h4>
-                            <p class="card-text text-center">{{ $template->description }}</p>
-                        </div>
-                        <div class="card-footer">
-                        <a href="{{ url('templates/' . $template->id) }}" class="btn btn-lg btn-block btn-info">Learn More!</a>
-                        </div>
-                    </div><!-- /.card -->
-                </div><!-- /.col-md-4 -->
-                @endforeach
-            </div><!-- /.row -->
-        </div><!-- /.card-->
-    </div><!-- /.row --> 
+        </div><!-- /.row -->
+        <br>
+        <div class="row my-3 templateOpacity">
+            <div class="card p-3">
+                <div class="row">
+                    @foreach($templates as $template)
+                    <div class="col-lg-4 my-2">
+                        <div class="card h-100">
+                            <div class="card-header">
+                                <img img class="img-fluid img-thumbnail" src="{{ $template->imgUrl . $template->img1Name }}" alt="{{ $template->name }}">
+                            </div>
+                            <div class="card-body">
+                                <h4 class="card-title text-center">{{ $template->name }}</h4>
+                                <p class="card-text text-center">{{ $template->description }}</p>
+                            </div>
+                            <div class="card-footer">
+                            <a href="{{ url('templates/' . $template->id) }}" class="btn btn-lg btn-block btn-info">Learn More!</a>
+                            </div>
+                        </div><!-- /.card -->
+                    </div><!-- /.col-md-4 -->
+                    @endforeach
+                </div><!-- /.row -->
+            </div><!-- /.card-->
+        </div><!-- /.row -->         
+    </div>
 </div><!-- /.container -->
 @endsection
